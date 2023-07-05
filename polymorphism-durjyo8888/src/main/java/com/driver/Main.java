c79f5ef186f6ef9df9405f407d26dc01c419dfdc
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-07-05 20:34:39 +0530

    Add files via upload
    git init

---
+++ polymorphism-durjyo8888/src/main/java/com/driver/Main.java
@@ -0,0 +1,29 @@
+package com.driver;
+
+public class Main {
+  public static class Product{
+
+    public int product(int x, int y){
+      return x*y;
+    }
+
+    public int product(int x, int y, int z){
+      return x*y*z;
+    }
+    public double product(double x, double y){
+      return x*y;
+    }
+
+    public static void main(String[]args){
+      Product p = new Product();
+
+      p.product(1,2);
+
+      p.product(1,2,3);
+
+      p.product(1.5,2.5);
+
+
+    }
+  }
+}
